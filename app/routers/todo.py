31634a8eed7207ee0b0efd2754af1a7cf92d2926
Anthony Cruz <anthonyleierlw@gmail.com>
2024-11-13 11:46:29 -0300

    Endpoint para listagem de todos

--- app/routers/todo.py
+++ app/routers/todo.py
@@ -1,11 +1,12 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from app.database import get_session
 from app.models import Todo, User
-from app.schemas import TodoPublic, TodoSchema
+from app.schemas import FilterTodo, TodoList, TodoPublic, TodoSchema
 from app.security import get_current_user
 
 router = APIRouter(prefix='/todos', tags=['todos'])
@@ -22,3 +23,20 @@ def create_todo(todo: TodoSchema, user: CurrentUser, session: DatabaseSession):
     session.refresh(db_todo)
 
     return db_todo
+
+
+@router.get('/', response_model=TodoList)
+def read_todos(session: DatabaseSession, user: CurrentUser, filter_todos: Annotated[FilterTodo, Query()]):
+    query = select(Todo).where(Todo.user_id == user.id)
+
+    if filter_todos.title:
+        query = query.filter(Todo.title.contains(filter_todos.title))
+
+    if filter_todos.description:
+        query = query.filter(Todo.description.contains(filter_todos.description))
+
+    if filter_todos.state:
+        query = query.filter(Todo.state == filter_todos.state)
+
+    todos = session.scalars(query.offset(filter_todos.offset).limit(filter_todos.limit)).all()
+    return {'todos': todos}
--- tests/test_todos.py
+++ tests/test_todos.py
@@ -1,3 +1,19 @@
+import factory
+import factory.fuzzy
+
+from app.models import Todo, TodoState
+
+
+class TodoFactory(factory.Factory):
+    class Meta: # type: ignore
+        model = Todo
+
+    title = factory.Faker('text')
+    description = factory.Faker('text')
+    state = factory.fuzzy.FuzzyChoice(TodoState)
+    user_id = 1
+
+
 def test_create_todo(client, token):
     response = client.post(
         '/todos/',
@@ -9,3 +25,13 @@ def test_create_todo(client, token):
         },
     )
     assert response.json() == {'id': 1, 'title': 'Test todo', 'description': 'Test todo description', 'state': 'draft'}
+
+
+def test_list_todos_should_return_5_todos(session, client, user, token):
+    expected_todos = 5
+
+    session.bulk_save_objects(TodoFactory.create_batch(expected_todos, user_id=user.id))
+    session.commit()
+
+    response = client.get('/todos', headers={'Authorization': f'Bearer {token}'})
+    assert len(response.json()['todos']) == expected_todos
